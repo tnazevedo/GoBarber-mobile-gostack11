0fd1da68dbe1264525bdccba4bb906e2269682e2
Talita Azevedo <talita.azevedo360@gmail.com>
2020-07-02 18:11:37 -0300

    Estrutura da Criação

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
 
-import { Button, View } from 'react-native';
+import { useAuth } from '../../hooks/auth';
+import {
+  Container,
+  Header,
+  BackButton,
+  HeaderTitle,
+  UserAvatar,
+} from './styles';
 
-import { Container, Title } from './styles';
+interface RouteParams {
+  providerId: string;
+}
 
 const CreateAppointment: React.FC = () => {
-  return <View />;
+  const route = useRoute();
+  const { user } = useAuth();
+
+  const { providerId } = route.params as RouteParams;
+  const { goBack } = useNavigation();
+  const navigateBack = useCallback(() => {
+    goBack();
+  }, [goBack]);
+
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={navigateBack}>
+          <Icon name="chevron-left" size={24} color="#999591" />
+        </BackButton>
+        <HeaderTitle>Cabeleireiros</HeaderTitle>
+        <UserAvatar source={{ uri: user.avatar_url }} />
+      </Header>
+    </Container>
+  );
 };
 
 export default CreateAppointment;
--- src/pages/CreateAppointment/styles.ts
+++ src/pages/CreateAppointment/styles.ts
@@ -1,12 +1,27 @@
 import styled from 'styled-components/native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
-  background: #312e38;
   flex: 1;
-  justify-content: center;
 `;
-
-export const Title = styled.Text`
-  color: #fff;
-  font-size: 24px;
+export const Header = styled.View`
+  padding: 24px;
+  padding-top: ${getStatusBarHeight() + 24}px;
+  background: #28262e;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+export const BackButton = styled.TouchableOpacity``;
+export const HeaderTitle = styled.Text`
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Medium';
+  font-size: 20px;
+  margin-left: 16px;
+`;
+export const UserAvatar = styled.Image`
+  width: 56px;
+  height: 56px;
+  border-radius: 28px;
+  margin-left: auto;
 `;
